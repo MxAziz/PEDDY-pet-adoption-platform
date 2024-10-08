1d88164e2a03b1d58b72b8e275504205b80f04e6
MxAziz <hereabdulaziz@gmail.com>
2024-10-08 13:03:04 +0600

    load pet categories

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,24 @@
 const loadPetCategories = () => {
     fetch('https://openapi.programming-hero.com/api/peddy/categories')
         .then(res => res.json())
-        .then(data => displayPetCategories(data))
+        .then(data => displayPetCategories(data.categories))
         .catch(error => console.log(error));
 
 }
 
-const displayPetCategories = (data) => {
-    console.log(data);
+const displayPetCategories = (categories) => {
+    const petBtnContainer = document.getElementById('categories')
+    categories.forEach(item  => {
+        console.log(item);
+        const button = document.createElement('button');
+        button.classList = "btn btn-lg";
+        button.innerHTML = `
+            <img src="item.category_icon">
+
+        `
+        petBtnContainer.append(button);
+
+    });
 
 }
 
---
+++ test.js
@@ -0,0 +1,22 @@
+[
+    {
+        "id": 1,
+        "category": "Cat",
+        "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
+    },
+    {
+        "id": 2,
+        "category": "Dog",
+        "category_icon": "https://i.ibb.co.com/c8Yp1y7/dog.png"
+    },
+    {
+        "id": 3,
+        "category": "Rabbit",
+        "category_icon": "https://i.ibb.co.com/3hftmLC/rabbit.png"
+    },
+    {
+        "id": 4,
+        "category": "Bird",
+        "category_icon": "https://i.ibb.co.com/6HHZwfq/bird.png"
+    }
+]
