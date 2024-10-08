f70b2202cf77597c14a1f29547aa0fbb9664f4a0
MxAziz <hereabdulaziz@gmail.com>
2024-10-08 19:41:19 +0600

    successfully added all pets from the url

--- scripts/main.js
+++ scripts/main.js
@@ -8,14 +8,47 @@ const loadPetCategories = () => {
 
 }
 
+// (1) load all pets
+const loadAllPets = () => {
+    fetch('https://openapi.programming-hero.com/api/peddy/pets')
+        .then(res => res.json())
+        .then(data => displayAllPets(data.pets))
+        .catch(error => console.log(error));
+}
+
+// (1)display
+const displayAllPets = (pets) => {
+    const petsContainer = document.getElementById('pets');
+    pets.forEach(item  => {
+        const card = document.createElement('div')
+        card.classList = "card card-compact ";
+        card.innerHTML = `
+        <figure>
+            <img
+            src=${item.image}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        petsContainer.append(card);
+    });
+}
+
+// (3)display
 const displayPetCategories = (categories) => {
     const petBtnContainer = document.getElementById('categories')
     categories.forEach(item  => {
-        console.log(item);
         const button = document.createElement('button');
-        button.classList = "btn btn-lg";
+        button.classList = "btn btn-lg space-x-2 font-bold border-2 bg-white ";
         button.innerHTML = `
-            <img src="item.category_icon">
+            <img src=${item.category_icon}>
+            <p>${item.category}</p>
 
         `
         petBtnContainer.append(button);
@@ -28,3 +61,4 @@ const displayPetCategories = (categories) => {
 
 
 loadPetCategories();
+loadAllPets();
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// 3----
 [
     {
         "id": 1,
@@ -20,3 +21,16 @@
         "category_icon": "https://i.ibb.co.com/6HHZwfq/bird.png"
     }
 ]
+// -----1-------
+[   {
+    "petId": 2,
+    "breed": "Siamese",
+    "category": "Cat",
+    "date_of_birth": "2022-09-05",
+    "price": 800,
+    "image": "https://i.ibb.co.com/3Wzz41D/pet-2.jpg",
+    "gender": "Female",
+    "pet_details": "This affectionate female Siamese cat is known for her vocal nature and love for attention. Born on September 5, 2022, she enjoys interactive play and snuggles. Fully vaccinated and priced at $800, she's the perfect fit for cat lovers who appreciate an intelligent, engaging, and sociable feline companion.",
+    "vaccinated_status": "Fully",
+    "pet_name": "Mia"
+    }                     ]
