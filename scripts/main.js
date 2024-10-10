2e415c757dbeccd7dd9beffc5417171f5e02267c
MxAziz <hereabdulaziz@gmail.com>
2024-10-11 03:36:10 +0600

    successfully added display all pets by category and error message

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@ const loadPetCategories = () => {
         .then(res => res.json())
         .then(data => displayPetCategories(data.categories))
         .catch(error => console.log(error));
-
 }
 
 // (1) load all pets
@@ -19,6 +18,22 @@ const loadAllPets = () => {
 // (1)display
 const displayAllPets = (pets) => {
     const petsContainer = document.getElementById('pets');
+    petsContainer.innerHTML = "";
+    if (pets.length === 0) {
+        petsContainer.classList.remove('grid');
+        petsContainer.innerHTML = `
+        <div class="flex flex-col gap-5 justify-center items-center text-center bg-[#13131308] p-20 rounded-2xl">
+            <img src="./assets/error.webp">
+            <h3 class="text-3xl font-bold">No Information Available</h3>
+            <p class="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at <br>
+            its layout. The point of using Lorem Ipsum is that it has a.</p>
+        </div>
+        `;
+        return;
+    } else {
+        petsContainer.classList.add('grid');
+    }
+
     pets.forEach(item => {
         const {breed, date_of_birth, gender, price } = item;
         const card = document.createElement('div')
@@ -49,24 +64,42 @@ const displayAllPets = (pets) => {
     });
 }
 
+// (4) load pets by category
+const loadCategoryPets = (id) => {
+    // alert(id);
+    // console.log(id);
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+        .then(res => res.json())
+        .then(data => displayAllPets(data.data))
+        .catch(error => console.log(error));
+
+}
+
 // (3)display
 const displayPetCategories = (categories) => {
     const petBtnContainer = document.getElementById('categories')
-    categories.forEach(item  => {
-        const button = document.createElement('button');
-        button.classList = "btn btn-lg rounded-xl space-x-2 font-bold border-2 bg-white ";
-        button.innerHTML = `
-            <img class="size-6 md:size-7" src=${item.category_icon}>
-            <p>${item.category}</p>
+    categories.forEach(item => {
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML = `
 
-        `
-        petBtnContainer.append(button);
+        <button onclick="loadCategoryPets('${item.category}')" class="btn btn-lg rounded-xl space-x-2 font-bold border-2 bg-white w-full">
+        <img class="size-6 md:size-7" src=${item.category_icon}>
+        <p>${item.category}</p>
+        </button>
+        `;
+
+        // button.classList = "btn btn-lg rounded-xl space-x-2 font-bold border-2 bg-white ";
+        // button.innerHTML = `
+        //     <img class="size-6 md:size-7" src=${item.category_icon}>
+        //     <p>${item.category}</p>
+
+        // `
+        // add btn to category container
+        petBtnContainer.append(buttonContainer);
 
     });
 
 }
-
-
 
 
 loadPetCategories();
